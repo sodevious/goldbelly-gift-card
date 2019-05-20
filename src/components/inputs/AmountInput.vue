a20b641cf42d4d2b5a645069405232b6f122d86f
Nicole Dominguez <sodevious.net@gmail.com>
2019-05-20 17:55:59 -0400

    Work on input components

---
+++ src/components/inputs/AmountInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="text" class="gb-giftcard-input" />
+</template>
+
+<script>
+  export default {
+    name: 'AmountInput'
+  }
+</script>
+
+<style>
+.gb-giftcard-input {
+  display: inline-block;
+  width: 50%;
+  padding: 1rem;
+  font-weight: 700;
+  font-size: 2rem;
+  font-family: var(--font-family-futura);
+  text-align: center;
+  background: transparent;
+  border: 1px solid transparent;
+  border-radius: 0.15rem;
+  outline: none;
+}
+
+.gb-giftcard-input:hover,
+.gb-giftcard-input:focus {
+  background-color: var(--color-white);
+  border: 1px solid var(--color-beige-dark);
+}
+</style>
---
+++ src/components/inputs/FormInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <p class="gb-form-row">
+    <label class="gb-form-label" :for="name" :aria-labelledby="name">
+      <slot></slot>
+    </label>
+
+    <textarea v-if="type === 'textarea'"
+              class="gb-form-input"
+              rows="3"
+              :id="name"
+              :placeholder="placeholder"></textarea>
+
+    <input v-else
+           class="gb-form-input"
+           :id="name"
+           :name="name"
+           :type="type"
+           :placeholder="placeholder" />
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'FormInput',
+    props: {
+      name: String,
+      type: String,
+      placeholder: String
+    }
+  }
+</script>
+
+<style>
+  .gb-form-row {
+    display: flex;
+  }
+
+  .gb-form-label {
+    flex-basis: 30%;
+    margin-right: 1rem;
+    padding-top: 0.5rem;
+    color: var(--color-red);
+    font-size: 1.05rem;
+    font-family: var(--font-family-futura);
+    text-align: left;
+  }
+
+  .gb-form-input {
+    flex: 1;
+    padding: 0.5rem;
+    font-size: 1.05rem;
+    font-family: var(--font-family-futura);
+    border: 0;
+    border-bottom: 1px dotted var(--color-beige-dark);
+  }
+</style>
