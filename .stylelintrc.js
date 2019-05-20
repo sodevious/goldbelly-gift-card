878345332cb36521deaa80627925be38164d4447
Nicole Dominguez <sodevious.net@gmail.com>
2019-05-20 15:20:07 -0400

    Stylelint and related stylelint plugins

---
+++ .stylelintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  "extends": "stylelint-config-rational-order",
+  "rules": {
+    "indentation": "tab",
+    "unit-blacklist": ["px"],
+    "indentation": 2,
+    "no-duplicate-selectors": true,
+    "selector-no-qualifying-type": true,
+    "selector-combinator-space-after": "always",
+    "selector-attribute-quotes": "always",
+    "selector-attribute-operator-space-before": "always",
+    "declaration-block-trailing-semicolon": "always",
+    "declaration-no-important": true,
+    "declaration-colon-space-before": "never",
+    "declaration-colon-space-after": "always",
+    "property-no-vendor-prefix": true,
+    "function-url-quotes": "always",
+    "font-weight-notation": "numeric",
+    "font-family-name-quotes": "always-unless-keyword",
+    "at-rule-no-vendor-prefix": true,
+    "rule-empty-line-before": "always-multi-line",
+    "selector-pseudo-element-colon-notation": "single",
+    "selector-pseudo-class-parentheses-space-inside": "never",
+    "selector-no-vendor-prefix": true,
+    "media-feature-range-operator-space-before": "always",
+    "media-feature-range-operator-space-after": "always",
+    "media-feature-parentheses-space-inside": "never",
+    "media-feature-name-no-vendor-prefix": true,
+    "media-feature-colon-space-after": "always"
+  }
+}
